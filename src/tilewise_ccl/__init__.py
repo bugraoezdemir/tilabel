ab8da2bad125fdc4e38eb5170fdbb100d12b252a
Bugra Oezdemir <oezdemir@embl.de>
2026-08-20 00:08:22 +0200

    v0.0.4

--- src/tilewise_ccl/__init__.py
+++ src/tilewise_ccl/__init__.py
@@ -10,7 +10,7 @@ downstream dask operations.
 pre-allocated output buffer); kept for comparison/benchmarking.
 """
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __author__ = "Bugra Oezdemir"
 
 from tilewise_ccl.core import label_array
